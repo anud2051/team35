ec9e261a56a6c07a8b81b6fc32a3b8f0a5207779
ogrendaniel <69965749+ogrendaniel@users.noreply.github.com>
2022-02-28 10:31:38 +0100

    updated play with isCheckMate

--- Play.hs
+++ Play.hs
@@ -36,21 +36,26 @@ main = play (initialBoard, White)
 --play is the gameloop which will continue until the game is over
 play (gameState, color1) = do
     showBoard gameState
-    putStrLn $ "Which " ++ show color1 ++ " piece do you want to move?"
-    piece1 <- getLine 
-    print "Where do you want to move it?"
-    move1 <- getLine
-
-    if color1 == stringToSquare piece1 && (validMove gameState (read piece1 :: Square) (read move1 :: Position)) && not (isCheck (Piece King (whereIsKing ((movePiece (read piece1::Square)) (read move1 :: Position) gameState) (Piece King (1,1) color1)) color1) ((movePiece (read piece1::Square) (read move1 :: Position) gameState)))
-    then 
-      if queening (read piece1 :: Square) (read move1 :: Position) 
-      then do
-      newPiece <- choosePiece ((read move1 :: Position), color1)
-      play $ (movePiece newPiece (read move1 :: Position) gameState, oppositeColor color1) 
-      else play $ (movePiece (read piece1::Square) (read move1 :: Position) gameState, oppositeColor color1)
+    if isCheckMate (Piece King (whereIsKing gameState (Piece King (1,1) color1)) color1)  gameState then 
+      putStrLn $ "Player " ++ show (oppositeColor color1) ++ " has won the game"
+--      quit
     else do
-    print "Incorrect move" 
-    play (gameState, color1)
+      putStrLn $ "Which " ++ show color1 ++ " piece do you want to move?"
+      piece1 <- getLine 
+      print "Where do you want to move it?"
+      move1 <- getLine
+  
+      if color1 == stringToSquare piece1 && (validMove gameState (read piece1 :: Square) (read move1 :: Position)) && not (isCheck (Piece King (whereIsKing ((movePiece (read piece1::Square)) (read move1 :: Position) gameState) (Piece King (1,1) color1)) color1) ((movePiece (read piece1::Square) (read move1 :: Position) gameState)))
+      then 
+        if queening (read piece1 :: Square) (read move1 :: Position) 
+        then do
+        newPiece <- choosePiece ((read move1 :: Position), color1)
+        play $ (movePiece newPiece (read move1 :: Position) gameState, oppositeColor color1) 
+        else play $ (movePiece (read piece1::Square) (read move1 :: Position) gameState, oppositeColor color1)
+      else do
+      print "Incorrect move" 
+      play (gameState, color1)
+
 
 stringToSquare string = color
   where 
