344fbdcd95408da5730a6e7fc47c7afb41a87988
Anik <anikuddin0231@gmail.com>
2022-03-01 08:56:32 +0100

    lol

--- Graphics.hs
+++ Graphics.hs
@@ -2,11 +2,4 @@ import Play
 import Graphics.Gloss
 
 
-main :: IO ()
-main = display window background drawing
-    where
-      window = InWindow "Nice Window" (200, 200) (0, 0) 
-      background = white 
-      drawing = Circle 80
-
 
